9671be77b2d4cb68c14e577f135217ba16e9f429
Henri Cavalcante <contato@henrimichel.com.br>
2015-02-15 18:13:49 -0300

    execute working by param

--- rc-module.ino
+++ rc-module.ino
@@ -18,7 +18,7 @@
  
  */
 
-#define DEBUG true
+#define DEBUG false
 #define STATICIP true
 #define RECEIVER_PIN 0 // Receiver on inerrupt 0 => that is pin #2
 #define TRANSMITTER_PIN 7
@@ -94,250 +94,209 @@ void loop() {
     mySwitch.resetAvailable();
   }
   else {
+    EthernetBonjour.run();
+    
+    char clientline[BUFSIZE];
+    int index = 0;
+    // listen for incoming clients
+    EthernetClient client = server.available();
 
-  // needed to continue Bonjour/Zeroconf name registration
-  EthernetBonjour.run();
-  
-  char clientline[BUFSIZE];
-  int index = 0;
-  // listen for incoming clients
-  EthernetClient client = server.available();
+    if (client) {
 
-  if (client) {
+      //  reset input buffer
+      index = 0;
 
-    //  reset input buffer
-    index = 0;
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
 
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
+          //  fill url the buffer
+          if(c != '\n' && c != '\r' && index < BUFSIZE){ // Reads until either an eol character is reached or the buffer is full
+            clientline[index++] = c;
+            continue;
+          }  
 
-        //  fill url the buffer
-        if(c != '\n' && c != '\r' && index < BUFSIZE){ // Reads until either an eol character is reached or the buffer is full
-          clientline[index++] = c;
-          continue;
-        }  
+  #if DEBUG
+          Serial.print("client available bytes before flush: "); Serial.println(client.available());
+          Serial.print("request = "); Serial.println(clientline);
+  #endif
 
-#if DEBUG
-        Serial.print("client available bytes before flush: "); Serial.println(client.available());
-        Serial.print("request = "); Serial.println(clientline);
-#endif
+          // Flush any remaining bytes from the client buffer
+          client.flush();
 
-        // Flush any remaining bytes from the client buffer
-        client.flush();
-
-#if DEBUG
-        // Should be 0
-        Serial.print("client available bytes after flush: "); Serial.println(client.available());
-#endif
-
-        //  convert clientline into a proper
-        //  string for further processing
-        String urlString = String(clientline);
-
-
-        //  extract the operation
-        String op = urlString.substring(0,urlString.indexOf(' '));
-
-        //  we're only interested in the first part...
-        urlString = urlString.substring(urlString.indexOf('/'), urlString.indexOf(' ', urlString.indexOf('/')));
-
-        //  put what's left of the URL back in client line
-#if CASESENSE
-        urlString.toUpperCase();
-#endif
-        urlString.toCharArray(clientline, BUFSIZE);
-
-#if DEBUG
-        Serial.print("urlString: "); Serial.println(urlString);
-#endif
-
-        //  get the first two parameters
-        char *action = strtok(clientline,"/");
-        char *value = strtok(NULL,"/");
-        String strAction = String(action);
-        String strValue = String(value);
-
-        //  this is where we actually *do something*!
-        char outValue[10] = "MU";
-        String jsonOut = String();
-
-        if(action != NULL){
-#if DEBUG
-        Serial.print("action: "); Serial.println(action);
-        Serial.print("value: "); Serial.println(value);
-#endif         
-          if(value != NULL) {
-
-            if(strAction == String("LEARN")) {
-              if(strValue == String("START")) {
-                startRcLearning(client);
+  #if DEBUG
+          // Should be 0
+          Serial.print("client available bytes after flush: "); Serial.println(client.available());
+  #endif
+          //  convert clientline into a proper
+          //  string for further processing
+          String urlString = String(clientline);
+          //  extract the operation
+          String op = urlString.substring(0,urlString.indexOf(' '));
+          //  we're only interested in the first part...
+          urlString = urlString.substring(urlString.indexOf('/'), urlString.indexOf(' ', urlString.indexOf('/')));
+          //  put what's left of the URL back in client line
+  #if CASESENSE
+          urlString.toUpperCase();
+  #endif
+          urlString.toCharArray(clientline, BUFSIZE);
+  #if DEBUG
+          Serial.print("urlString: "); Serial.println(urlString);
+  #endif
+          char *action = strtok(clientline,"/");
+          char *value = strtok(NULL,"/");
+          char outValue[10] = "MU";
+          String jsonOut = String();
+          if(action != NULL){
+  #if DEBUG
+          Serial.print("action: "); Serial.println(action);
+          Serial.print("value: "); Serial.println(value);
+  #endif         
+            if(value != NULL) {
+              if(strcmp (action,"LEARN") == 0) {
+                if(strcmp (value,"START") == 0) {
+                  startRcLearning(client);
+                }
+                else if (strcmp (value,"STOP") == 0) {
+                  stopRcLearning(client);
+                }
               }
-              else if (strValue == String("STOP")) {
-                stopRcLearning(client);
+              else if (strcmp (action,"EXECUTE") == 0) {
+  #if DEBUG
+    Serial.println("execute mode activated");           
+  #endif
+                rcExecute(value);
+                
               }
-            }
-            else if (strAction == String("EXECUTE")) {
-#if DEBUG
-              Serial.print("execute mode activated");           
-#endif    
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-              rcExecute();
-            }
+              else {
+  #if DEBUG
+                //  set the pin value
+                Serial.println("setting pin");
+  #endif
+                //  select the pin
+                int selectedPin = atoi (action);
+  #if DEBUG
+                Serial.println(selectedPin);
+  #endif
+                //  set the pin for output
+                pinMode(selectedPin, OUTPUT);
+                //  determine digital or analog (PWM)
+                if(strncmp(value, "HIGH", 4) == 0 || strncmp(value, "LOW", 3) == 0) {
+  #if DEBUG
+                  //  digital
+                  Serial.println("digital");
+  #endif
+
+                  if(strncmp(value, "HIGH", 4) == 0) {
+  #if DEBUG
+                    Serial.println("HIGH");
+  #endif
+                    digitalWrite(selectedPin, HIGH);
+                  }
+                  if(strncmp(value, "LOW", 3) == 0) {
+  #if DEBUG
+                    Serial.println("LOW");
+  #endif
+                    digitalWrite(selectedPin, LOW);
+                  }
+                } 
+                else {
+  #if DEBUG
+                  //  analog
+                  Serial.println("analog");
+  #endif
+                  //  get numeric value
+                  int selectedValue = atoi(value);              
+  #if DEBUG
+                  Serial.println(selectedValue);
+  #endif
+                  analogWrite(selectedPin, selectedValue);
+                }
+                showSuccess(client, jsonOut);
+              }
+            } 
             else {
-#if DEBUG
-              //  set the pin value
-              Serial.println("setting pin");
-#endif
-              //  select the pin
-              int selectedPin = atoi (action);
-#if DEBUG
-              Serial.println(selectedPin);
-#endif
+  #if DEBUG
+              //  read the pin value
+              Serial.println("reading pin");
+  #endif
 
-              //  set the pin for output
-              pinMode(selectedPin, OUTPUT);
+              char* pin = action;
+              //  determine analog or digital
+              if(pin[0] == 'a' || pin[0] == 'A'){
 
-              //  determine digital or analog (PWM)
-              if(strncmp(value, "HIGH", 4) == 0 || strncmp(value, "LOW", 3) == 0) {
+                //  analog
+                int selectedPin = pin[1] - '0';
 
-#if DEBUG
-                //  digital
-                Serial.println("digital");
-#endif
+  #if DEBUG
+                Serial.println(selectedPin);
+                Serial.println("analog");
+  #endif
 
-                if(strncmp(value, "HIGH", 4) == 0) {
-#if DEBUG
-                  Serial.println("HIGH");
-#endif
-                  digitalWrite(selectedPin, HIGH);
-                }
+                sprintf(outValue,"%d",analogRead(selectedPin));
 
-                if(strncmp(value, "LOW", 3) == 0) {
-#if DEBUG
-                  Serial.println("LOW");
-#endif
-                  digitalWrite(selectedPin, LOW);
-                }
+  #if DEBUG
+                Serial.println(outValue);
+  #endif
 
               } 
-              else {
+              else if(pin[0] != NULL) {
 
-#if DEBUG
-                //  analog
-                Serial.println("analog");
-#endif
-                //  get numeric value
-                int selectedValue = atoi(value);              
-#if DEBUG
-                Serial.println(selectedValue);
-#endif
-                analogWrite(selectedPin, selectedValue);
+                //  digital
+                int selectedPin = pin[0] - '0';
 
+  #if DEBUG
+                Serial.println(selectedPin);
+                Serial.println("digital");
+  #endif
+
+                pinMode(selectedPin, INPUT);
+
+                int inValue = digitalRead(selectedPin);
+
+                if(inValue == 0){
+                  sprintf(outValue,"%s","LOW");
+                  //sprintf(outValue,"%d",digitalRead(selectedPin));
+                }
+
+                if(inValue == 1){
+                  sprintf(outValue,"%s","HIGH");
+                }
+
+  #if DEBUG
+                Serial.println(outValue);
+  #endif
               }
 
-              
+              //  assemble the json output
+              jsonOut += "{\"";
+              jsonOut += pin;
+              jsonOut += "\":\"";
+              jsonOut += outValue;
+              jsonOut += "\"}";
+
               showSuccess(client, jsonOut);
-
             }
-
           } 
           else {
-#if DEBUG
-            //  read the pin value
-            Serial.println("reading pin");
-#endif
 
-            char* pin = action;
-            //  determine analog or digital
-            if(pin[0] == 'a' || pin[0] == 'A'){
+            //  error
+            showError(client);
 
-              //  analog
-              int selectedPin = pin[1] - '0';
-
-#if DEBUG
-              Serial.println(selectedPin);
-              Serial.println("analog");
-#endif
-
-              sprintf(outValue,"%d",analogRead(selectedPin));
-
-#if DEBUG
-              Serial.println(outValue);
-#endif
-
-            } 
-            else if(pin[0] != NULL) {
-
-              //  digital
-              int selectedPin = pin[0] - '0';
-
-#if DEBUG
-              Serial.println(selectedPin);
-              Serial.println("digital");
-#endif
-
-              pinMode(selectedPin, INPUT);
-
-              int inValue = digitalRead(selectedPin);
-
-              if(inValue == 0){
-                sprintf(outValue,"%s","LOW");
-                //sprintf(outValue,"%d",digitalRead(selectedPin));
-              }
-
-              if(inValue == 1){
-                sprintf(outValue,"%s","HIGH");
-              }
-
-#if DEBUG
-              Serial.println(outValue);
-#endif
-            }
-
-            //  assemble the json output
-            jsonOut += "{\"";
-            jsonOut += pin;
-            jsonOut += "\":\"";
-            jsonOut += outValue;
-            jsonOut += "\"}";
-
-            showSuccess(client, jsonOut);
           }
-        } 
-        else {
-
-          //  error
-          showError(client);
-
+          break;
         }
-        break;
       }
+    closeConnection(client);
+      
     }
-  closeConnection(client);
-    
-  }
 
   }
 }
 
 void startRcLearning(EthernetClient client) {
 #if DEBUG
-  Serial.print("learn mode activated"); 
+  Serial.println("learn mode activated"); 
 #endif
   rcMode = 'l';
   rcModeTick = 1000;
@@ -351,7 +310,7 @@ void startRcLearning(EthernetClient client) {
 
 void stopRcLearning(EthernetClient client) {
 #if DEBUG
-  Serial.print("learn mode deactivated"); 
+  Serial.println("learn mode deactivated"); 
 #endif
   rcMode = 's';
   rcModeTick = 0;
@@ -379,7 +338,6 @@ void writeBufferLearning(unsigned int length, unsigned int* raw) {
 #endif
   }
 }
-
 
 void showError(EthernetClient client) {
 
@@ -423,7 +381,6 @@ void closeConnection(EthernetClient client) {
 }
 
 //let’s build our instruments:
-
 void customDelay(unsigned long time) {
     unsigned long end_time = micros() + time;
     while(micros() < end_time);
@@ -432,6 +389,12 @@ void customDelay(unsigned long time) {
 //1000=1ms
 
 void setStateWithDelay(int pin, int state,int delayTime) {
+#if DEBUG
+  Serial.print("state: ");
+  Serial.print(state);
+  Serial.print(" delayTime: ");
+  Serial.println(delayTime);
+#endif
   if(state==1)
     digitalWrite(pin,HIGH);
   else
@@ -440,11 +403,32 @@ void setStateWithDelay(int pin, int state,int delayTime) {
   customDelay(delayTime);
 }
 
-void rcExecute() {
+void rcExecute(char strCode[]) {
 
+  char* strDelayTime = strtok(strCode,"-");
+  char* codes = strtok(NULL,"-");
+  //String strCodes = String(codes);
+  int delayTime = atoi (strDelayTime);
+  //int startDelayTime = strtok(NULL,",");
+#if DEBUG
+  Serial.println("Delay: ");
+  Serial.println(delayTime);
+  Serial.println("Codes: ");
+  Serial.println(codes);
+  Serial.println("Size: ");
+  Serial.println(strlen(codes));
 
-setStateWithDelay(TRANSMITTER_PIN,0,976);
+#endif
 
+for(int j=0; j<10; j++){
+  setStateWithDelay(TRANSMITTER_PIN,0,11284);
 
+  for (int i = 0; i < strlen(codes) ; i++) {
+    int code = (codes[i] - '0') + 1;
+    setStateWithDelay(TRANSMITTER_PIN,(i + 1) % 2,delayTime * code);
+  }
   digitalWrite(TRANSMITTER_PIN,LOW);
+}
+
+
 }
